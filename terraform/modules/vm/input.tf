a6729aa9d533df2f0f3bd514bc998d06bc658382
Marko Njegovan <marko.njegovan@gmail.com>
2021-06-23 10:43:36 +0200

    first commit

---
+++ terraform/modules/vm/input.tf
@@ -0,0 +1,8 @@
+
+variable "public_ip" {}
+variable "resource_group" {}
+variable "location" {}
+variable "name" {}
+variable "subnet_id" {}
+variable "admin_username" {}
+variable "public_key_path" {}
